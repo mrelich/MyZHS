800457863c6eea9176dd44bcdc5e7924c7f73b86
mrelich <mrelich6@gmail.com>
2014-05-07 17:02:18 +0900

    updating runZHS.py

--- runZHS.py
+++ runZHS.py
@@ -13,14 +13,19 @@
 from ROOT import TRandom3
 from subprocess import Popen
 import os
+import stat
 
 #---------------------------------#
 # User specifications here
 #---------------------------------#
 
-NEvents = 10
-random  = TRandom3()
-jobOutput = "output.txt"
+NEvents   = 1 #00
+beamE     = "3e12" # MeV
+outDir    = "beam"+beamE+"MeV/"
+jobOutput = "output_beam"+beamE+".txt"
+radLengths= 0.92 * 40 * 100
+
+print radLengths
 
 #
 ## Useful method
@@ -28,6 +33,28 @@ jobOutput = "output.txt"
 def n():
     return "\n"
 
+#
+## Some variable defs
+#
+random    = TRandom3()
+tempName  = "template_"+beamE+".inp"
+beamName  = "beam_"+beamE+".inp"
+
+#---------------------------------#
+# Method to build beam template.
+# Right now just single particle. 
+#---------------------------------#
+def beamTemplate(energy):
+    out = open(beamName,"w")
+    out.write("1. ")
+    out.write("1 ")
+    out.write(energy+" ")   # particle energy
+    out.write("0. ")              # x-position
+    out.write("0. ")              # y-position
+    out.write("0. ")              # z-position
+    out.write(n())
+    out.close()
+    return
 
 #---------------------------------#
 # Define method to build template
@@ -35,9 +62,8 @@ def n():
 # to be configurable. This is based
 # on template_ice.inp
 #---------------------------------#
-
 def buildTemplate(randNum, outName):
-    out = open("template.inp","w")
+    out = open(tempName,"w")
     out.write("ice.med"+n())
     out.write(str(randNum)+n())
     out.write("-1"+n())
@@ -49,7 +75,7 @@ def buildTemplate(randNum, outName):
     out.write("1"+n())
     out.write("0. 0."+n())
     out.write("1"+n())
-    out.write("92.4"+n())
+    out.write(str(radLengths) +n()) 
     out.write("1"+n())
     out.write("1"+n())
     out.write("1"+n())
@@ -57,22 +83,36 @@ def buildTemplate(randNum, outName):
     out.write("1"+n())
     out.write(outName+n())
     out.write("grid_frequency.inp"+n())
-    out.write("5  0. 0.1 -0.1 0.2 -0.2"+n())
+    out.write("5 \t0. \t0.1 \t-0.1 \t0.3 \t-0.3"+n())
     out.write("1 0."+n())
     out.write("1"+n())
-    out.write("0.12  0.12 0.0"+n())
+    #out.write("0.12  0.12 0.0"+n())
+    out.write("3.696  3.696 0.0"+n())
     out.write("0.0  0.0  1.0"+n())
     out.write("-1.0  0.0  0.0"+n())
-    out.write("template_beam.inp"+n())
+    #out.write("template_beam.inp"+n())
+    out.write(beamName + n())
     out.close()
+    #os.chmod(tempName,stat.S_IEXEC)
+    print "Built template: ", tempName
+    return
 
 #---------------------------------#
 # Loop and execute jobs
 #---------------------------------#
 
+# Check for job output location
 if os.path.exists(jobOutput):
     os.remove(jobOutput)
 
+# Make sure out directory exists
+if not os.path.exists(outDir):
+    os.mkdir(outDir)
+
+# Generate Beam template
+beamTemplate(beamE)
+
+# Loop
 for evt in range(0,NEvents):
 
     if evt % 20 == 0:
@@ -87,16 +127,19 @@ for evt in range(0,NEvents):
     eventName += str(evt)
 
     seed = int( 1000000 * random.Rndm() )
-
+    
     # Build the template
     buildTemplate(seed, eventName)
     
     # Execute
-    command = "./ZHS_time_freq_Fresnel_beam < template.inp >> " + jobOutput
+    command = "./ZHS_time_freq_Fresnel_beam < " + tempName + " >> " + jobOutput
     run     = Popen([command],shell=True)
     run.wait()
-    os.remove("template.inp")
+    os.remove(tempName)
 
     # Move output files
-    mv = Popen(["mv *.EMP output/ && mv *.out output/"],shell=True)
+    mv = Popen(["mv *.EMP " + outDir + "&& mv *.out " + outDir],shell=True)
     mv.wait()
+
+# Now remove beam template
+os.remove(beamName)
